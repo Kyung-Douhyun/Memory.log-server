b9fed0c314b4d39332cc222b9c67cb30fd5dd9e0
경두현 <douhyun.kyung@gmail.com>
2020-07-19 17:53:58 +0900

    :art: [server] app.js route setting
    app.js에서 클라이언트와 서버의 연결을 위한 라우트 초기 설정.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+const photoRouter = require('./routes/photo');
+const userRouter = require('./routes/user');
+
 const app = express();
 const port = 4000;
 
@@ -8,5 +11,10 @@ app.get('/', (req, res) =>
     res.status(200).send("OK")
     )
 
+app.use("/photo", photoRouter);
+app.use("/user", userRouter);
+
 app.listen(port, () => 
     console.log(`Example app listening at http://localhost:${port}`))
+
+module.exports = app;
