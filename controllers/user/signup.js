28b5e3699f943756634b6b05d50544ed90a58ae5
경두현 <douhyun.kyung@gmail.com>
2020-07-20 15:46:59 +0900

    :heavy_plus_sign: [server] aws s3 multer setting
    이미지 파일 업로드에 필요한 AWS와 멀터의 설정. awd-sdk, multer, multer-3 설치.

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -4,13 +4,13 @@ module.exports = {
 	post: (req, res) => {
 		// TODO: 유저가 회원가입을 했을 떄, 회원정보를 DB에 담아주는 부분
 		// s사용자가 회원가입할 때 적어야 할 정보는 name, email, password -> 요청(req)의 body 부분에 담길 내용
-		let { name, email, password } = req.body;
+		let { username, email, password } = req.body;
 
 		User.findOrCreate({
 			where: { email }, // 이메일이 중복되는지만 확인함
-			defaults: { name, password },
+			defaults: { username, password },
 		}).then(([user, created]) => {
-			if (created) {
+			if (!created) {
 				res.status(409).send('Existing user'); // 동일한 email이 존재하면 conflict code 409를 보내줌
 			}
 			const data = user.get({ plain: true });
